871711fca0a4cfdbbb44550907305110a1f48097
fastivetsv-star <fastivetsv@gmail.com>
2026-02-03 21:21:45 +0100

    add comments and store logic

--- lesson1.py
+++ lesson1.py
@@ -1,96 +1,64 @@
-# 1.1 
-def get_length(text):
-    return len(text)
-print(get_length("Hello World"))
+class Product:
+    # створили: имя категорію цціну та к-сть
+    def __init__(self, name, category, price , quantity, ):
+        self.name = name
+        self.category = category
+        self.price = price
+        self.quantity = quantity
+# показує інформацію яку ми впишем в зміну класу 
+    def information (self):
+        print(f'Продукт {self.name} категорія: {self.category} ціна {self.price} кількість {self.quantity} ')        
+#можна вписати нову ціну за продукту
+    def change_price(self, new_price):
+        self.price = new_price
+        print(f'Ціна на {self.name} змінена на {self.price}')
+# можна вписати нову к-сть продукту 
+    def update_quantity(self,amount):
+        self.quantity = amount
+        print(f'Нова кількість продукту {self.name}: {self.quantity}')
+# юзер блок 
+class Customer:
+    def __init__(self, name, email ,):
+        self.name = name
+        self.email = email
+        self.orders = []
+#коли клієнт робе заказ додаємо до пустого списку self.orders = [] пишем іім'я та суму
+    def add_order(self, order):
+        self.orders.append(order)
+        print(f"Клієнт {self.name} оформив замовлення. До сплати: {order.total_sum}")
+# сторюємо список покупок і зазначаємо що на даний момент ця сума 0      
+products_list=[]
+class Order:
+    def __init__(self):
+        self.products = []
+        self.total_sum = 0
+# добавляємо продукт який ми спишем в ... .add_product(якийсь продукт): , і додаємо до загальной суми
+    def add_product(self,product):
+        self.products.append(product)
+        self.total_sum = self.total_sum + product.price
+        print(f"До замовлення додано: {product.name}. Сума: {self.total_sum}")
 
-# 1.2 
-def combine_strings(text1, text2):
-    return text1 + text2
-print(combine_strings("Hello ", "World"))
+# читаємо тхт файл де будуть декілька товарів
+with open(r'product.txt', 'r', encoding='utf-8') as file:
+    for i in file:
+        data = i.strip().split(',') #розділяємо інформацію через кому 
+        new_product = Product(data[0],data[1],float(data[2]),float(data[3]))#кажемо що буде 4 різних даних name, category, price , quantity
+        products_list.append(new_product)#додаємо в список який раніше стврорили 
 
-# 2.1 
-def get_square(num):
-    return num ** 2
-print( get_square(5))
+# відображення інфи
+my_toy =Product('Свинка ','іграшка', 500, 10)
+my_toy.change_price(600) #змінено ціну
+my_toy.update_quantity(21) #змінено к-сть
+my_toy.information()
 
-# 2.2 
-def sum_numbers(a, b):
-    return a + b
-print(sum_numbers(33, 22))
+# звертаємось до списку покупок 
+my_order = Order()
+first_toy = products_list[0] #робимо першу покупку зазначаємо що купляємо товар ведмедик який в тхт файлі під номером 0
+my_order.add_product(first_toy)#через ад продук додаємо перший продукт щоб сума додалась до 0 
+second_toy = products_list[1]
+my_order.add_product(second_toy)
 
-# 2.3 
-
-def divide_full(a, b):
-    integer_part = a // b
-    remainder = a % b
-    return integer_part, remainder
-print(divide_full(25, 5))
-
-# 3.1
-def average_value(numbers):
-    if len(numbers) == 0: return 0 
-    return sum(numbers) / len(numbers)
-my_list = [10, 5, 12, 3, 8]
-print(average_value(my_list))
-
-# 3.2 
-def common_elements(list1, list2):
-    return list(set(list1) & set(list2))
-lst1 = [5, 7, 9, 0, 1, 4]
-lst2 = [1, 2, 5, 6, 8, 1, 0]
-print(common_elements(lst1, lst2))
+client = Customer("Владислав", "vlad@email.com")
+client.add_order(my_order)
 
 
-
-# 4.1 
-def print_keys(data_dict):
-    for key in data_dict:
-        print(key)
-person = {'name': 'Kolia', 'age': '22', 'surname': 'shewcenko'}
-print(" Ключі:")
-print_keys(person)
-
-# 4.2 
-def merge_dictionaries(d1, d2):
-    return d1 | d2 
-print_keys(person)
-info1 = {'name': 'Kolia', 'age': '33'}
-info2 = {'city': 'Kyiv', 'job': 'Driver'} 
-print(merge_dictionaries(info1, info2))
-
-# 5.1 
-def union_sets(set1, set2):
-    return set1 | set2
-# 5.2
-def check_subset(subset, main_set):
-    return subset.issubset(main_set)
-s1 = {1, 2, 3}
-s2 = {3, 4, 5}
-s_small = {1, 2}
-print( union_sets(s1, s2))
-print( check_subset(s_small, s1))
-# 6.1
-def check_odd_even(number):
-    if number % 2 == 0:
-        print(f"{number} - Парне")
-    else:
-        print(f"{number} - Непарне")
-
-# 6.2
-def get_even_numbers(numbers_list):
-    result = []
-    for num in numbers_list:
-        if num % 2 == 0:
-            result.append(num) 
-    return result
-
-check_odd_even(10)
-check_odd_even(7)
-
-numbers = [1, 2, 3, 4, 5, 6, 10, 11]
-print("Тільки парні:", get_even_numbers(numbers))
-# 7
-check_parity = lambda x: "парне" if x % 2 == 0 else "не парне"
-
-print(check_parity(4)) 
-print(check_parity(9))  
